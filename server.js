38fe35685babbbc0e349a2e82bada9f29dabee4a
Kevin <kan.kevin@gmail.com>
2017-03-29 21:13:18 -0400

    Query database

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const db = require('./db.js');
 
 const server = express();
 const port = 5000;
 
-// Route
-server.get('/api', (req, res) => {
+// Simple route
+server.get('/api/hello', (req, res) => {
   const resObj = {
     data: 'Hello from the back-end!',
   };
   return res.status(200).send(resObj);
+});
+
+// Route that queries database
+server.get('/api/games', (req, res) => {
+  const query = 'SELECT * FROM game_results';
+  db.query(query, [], (err, rows) => {
+    if (err) { return res.status(500).send(err); }
+    return res.status(200).send(rows);
+  });
 });
 
 // Listen for requests
