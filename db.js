b70272a5f56ce974e4581f6c8c3be73ef0abce4a
Kevin <kan.kevin@gmail.com>
2017-04-05 23:50:30 -0400

    Create promises for scraper

--- db.js
+++ db.js
@@ -1,18 +1,24 @@
-// db.js centralizes access to the database by:
-// 1. Creating a global pool of database clients
-// 2. Exporting a method for passing queries to the pool
+/**
+ * db.js centralizes access to the database by:
+ * 1. Creating a global pool of database clients
+ * 2. Exporting a method for passing queries to the pool
+ */
 
 const config = require('./config');
 const pg = require('pg');
 const url = require('url');
 
-// By default, node-postgres interprets incoming timestamps in the local timezone
-// Force node-postgres to interpret the incoming timestamps without any offsets,
-// since our queries will select timestamps in the desired timezone
+/**
+ * By default, node-postgres interprets incoming timestamps in the local timezone
+ * Force node-postgres to interpret the incoming timestamps without any offsets,
+ * since our queries will select timestamps in the desired timezone
+ */
 pg.types.setTypeParser(1114, stringValue => new Date(Date.parse(`${stringValue}+0000`)));
 
-// Configure and initialize the Postgres connection pool
-// Get the DATABASE_URL config var and parse it into its components
+/**
+ * Configure and initialize the Postgres connection pool
+ * Get the DATABASE_URL config var and parse it into its components
+ */
 const params = url.parse(config.DATABASE_URL);
 const authParams = params.auth.split(':');
 const pgConfig = {
@@ -29,6 +35,8 @@ const pgConfig = {
 // Initialize a global connection pool
 const pool = new pg.Pool(pgConfig);
 
-// Export query method for passing queries to the pool
-// `values` is an array of values for parameterized queries
+/**
+ * Export query method for passing queries to the pool
+ * `values` is an array of values for parameterized queries
+ */
 module.exports.query = (text, values) => pool.query(text, values);
--- scripts/scrape-games.js
+++ scripts/scrape-games.js
@@ -1,55 +1,73 @@
 const request = require('request');
 
-const gids = [2016020001, 2016020002];
-
-// Return a promise for a json object
-function promisifyRequest(url) {
-  return new Promise((resolve, reject) => {
-    request(url, (err, res, body) => {
-      if (err) {
-        return reject(err);
-      } else if (res.statusCode !== 200) {
-        const rejErr = new Error(`Unexpected status code: ${res.statusCode}`);
-        rejErr.res = res;
-        return reject(rejErr);
-      }
-      return resolve(JSON.parse(body));
-    });
-  });
-}
-
-// Return a promise for both the pbp and shift json objects
+/**
+ * Return a Promise.all whose iterable contains:
+ * 0. A promise for the specified game's pbp data
+ * 1. A promise for the specified game's shift data
+ */
 function fetchNhlData(gid) {
   const pbpUrl = `https://statsapi.web.nhl.com/api/v1/game/${gid}/feed/live`;
-  const pbpPromise = promisifyRequest(pbpUrl);
   const shiftUrl = `http://www.nhl.com/stats/rest/shiftcharts?cayenneExp=gameId=${gid}`;
-  const shiftPromise = promisifyRequest(shiftUrl);
-  return Promise.all([pbpPromise, shiftPromise]);
+  const promises = [pbpUrl, shiftUrl].map(url =>
+    new Promise((resolve, reject) => {
+      request(url, (err, res, body) => {
+        if (err) {
+          return reject(err);
+        } else if (res.statusCode !== 200) {
+          const rejErr = new Error(`Unexpected status code: ${res.statusCode}`);
+          rejErr.res = res;
+          return reject(rejErr);
+        }
+        return resolve(JSON.parse(body));
+      });
+    }) // eslint-disable-line comma-dangle
+  );
+  return Promise.all(promises);
 }
 
-// Process the nhl data and write results to database
-function processData(pbpData, shiftData) {
-  console.log(pbpData);
-  console.log(shiftData);
+/**
+ * Given the nhl's pbp and shift data,
+ * return an object containing the rows to be inserted
+ */
+function processData(pbpJson, shiftJson) {
+  return {
+    table1Rows: ['a', 'b', 'c', pbpJson.gamePk],
+    table2Rows: ['x', 'y', 'z', shiftJson.data.length],
+  };
 }
 
-// Scrape data for the first game id in `gids`
-// This function calls itself to iterate through an array of game ids
-function scrapeFirstInArray() {
-  fetchNhlData(gids[0]).then((responses) => {
-    processData(responses[0], responses[1]);
-    console.log(`Finished processing ${gids[0]} at ${new Date()}`);
+/**
+ * Scrape data for an array of game ids by:
+ * 1. Fetching the nhl pbp and shift data for the game id at the specified index
+ * 2. Generating database rows to be inserted
+ * 3. Inserting the database rows
+ * 4. Repeating for the next game id
+ */
+function scrapeGame(gids, idx) {
+  fetchNhlData(gids[idx])
+    .then((data) => {
+      // Process the fetched data
+      const rowsToInsert = processData(data[0], data[1]);
+      return rowsToInsert;
+    })
+    .then((rowsToInsert) => {
+      // Insert rows into database
+      console.log(`Inserting rows for ${gids[idx]}`);
+      console.log(rowsToInsert);
+      return 'Rows inserted';
+    })
+    .then((insertResult) => {
+      console.log(insertResult);
 
-    // Scrape next game by shifting the array (what was previously the 2nd element becomes the 1st)
-    // Use a delay to space out api requests and database writes
-    gids.shift();
-    if (gids.length > 0) {
-      console.log(`Started scraping ${gids[0]} at ${new Date()}`);
-      setTimeout(() => scrapeFirstInArray(), 1000);
-    } else {
-      console.log('Scrape finished');
-    }
-  });
+      // Scrape the next game
+      if (idx < gids.length - 1) {
+        scrapeGame(gids, idx + 1);
+      } else {
+        console.log('Finished scraping games');
+      }
+    });
 }
 
-scrapeFirstInArray();
+// Specify game ids and start scraping from the first game id
+const gids = [2016020001, 2016020002];
+scrapeGame(gids, 0);
